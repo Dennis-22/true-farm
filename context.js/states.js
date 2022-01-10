e197e75c1459ccb6772b2ddb469a0878ba2ecc44
Dennis Jeminal <dennisjeminal8@gmail.com>
2022-01-09 22:03:08 -0500

    rooled back

--- context.js/states.js
+++ context.js/states.js
@@ -33,100 +33,100 @@ const Data = [
         price:5
     },
 
-    // {
-    //     id:'3', 
-    //     category:'fruit',
-    //     name:'Pineapple',
-    //     image: pineapple,
-    //     price:15
-    // }, 
-    // {
-    //     id:'5', 
-    //     category:'fruit',
-    //     name:'Pawpaw',
-    //     image: pawpaw,
-    //     price:6
-    // },
-    // {
-    //         id:'7', 
-    //         category:'fruit',
-    //         name:'Apple',
-    //         image: apple,
-    //         price:17
-    //     },
-    // {
-    //     id:'2', 
-    //     category:'fruit',
-    //     name:'Orange',
-    //     image: orange,
-    //     price:7
-    // }, 
-    //     {
-    //     id:'9', 
-    //     category:'vegetable',
-    //     name:'Onion',
-    //     image: onion,
-    //     price:10
-    // },{
-    //         id:'12', 
-    //         category:'vegetable',
-    //         name:'Tomatoes',
-    //         image: tomatoes,
-    //         price:15
-    //     },
-    // {
-    //     id:'4', 
-    //     category:'fruit',
-    //     name:'Strawberry',
-    //     image: strawberry,
-    //     price:12
-    // }, 
-    //      {
-    //     id:'6', 
-    //     category:'fruit',
-    //     name:'Watermelon',
-    //     image: watermelon,
-    //     price:11
-    // },  {
-    //     id:'8', 
-    //     category:'fruit',
-    //     name:'Grapes',
-    //     image: grapes,
-    //     price:15
-    // }, 
+    {
+        id:'3', 
+        category:'fruit',
+        name:'Pineapple',
+        image: pineapple,
+        price:15
+    }, 
+    {
+        id:'5', 
+        category:'fruit',
+        name:'Pawpaw',
+        image: pawpaw,
+        price:6
+    },
+    {
+            id:'7', 
+            category:'fruit',
+            name:'Apple',
+            image: apple,
+            price:17
+        },
+    {
+        id:'2', 
+        category:'fruit',
+        name:'Orange',
+        image: orange,
+        price:7
+    }, 
+        {
+        id:'9', 
+        category:'vegetable',
+        name:'Onion',
+        image: onion,
+        price:10
+    },{
+            id:'12', 
+            category:'vegetable',
+            name:'Tomatoes',
+            image: tomatoes,
+            price:15
+        },
+    {
+        id:'4', 
+        category:'fruit',
+        name:'Strawberry',
+        image: strawberry,
+        price:12
+    }, 
+         {
+        id:'6', 
+        category:'fruit',
+        name:'Watermelon',
+        image: watermelon,
+        price:11
+    },  {
+        id:'8', 
+        category:'fruit',
+        name:'Grapes',
+        image: grapes,
+        price:15
+    }, 
 
-    // {
-    //     id:'10', 
-    //     category:'vegetable',
-    //     name:'Carrots',
-    //     image: carrots,
-    //     price:8
-    // }, {
-    //     id:'11', 
-    //     category:'vegetable',
-    //     name:'Letuce',
-    //     image: letuce,
-    //     price:13
-    // },   {
-    //     id:'14', 
-    //     category:'vegetable',
-    //     name:'Almonds',
-    //     image: almond,
-    //     price:16
-    // }, {
-    //     id:'15', 
-    //     category:'vegetable',
-    //     name:'Green Pea',
-    //     image: greenPea,
-    //     price:3
-    // },
-    // {
-    //     id:'16', 
-    //     category:'vegetable',
-    //     name:'Spinach',
-    //     image: spinach,
-    //     price:16
-    // },
+    {
+        id:'10', 
+        category:'vegetable',
+        name:'Carrots',
+        image: carrots,
+        price:8
+    }, {
+        id:'11', 
+        category:'vegetable',
+        name:'Letuce',
+        image: letuce,
+        price:13
+    },   {
+        id:'14', 
+        category:'vegetable',
+        name:'Almonds',
+        image: almond,
+        price:16
+    }, {
+        id:'15', 
+        category:'vegetable',
+        name:'Green Pea',
+        image: greenPea,
+        price:3
+    },
+    {
+        id:'16', 
+        category:'vegetable',
+        name:'Spinach',
+        image: spinach,
+        price:16
+    },
 ]
 
 
--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+// const withImages = require('next-images')
+// module.exports = withImages()
+
+
+// const withImages = require('next-images')
+// module.exports = withImages({
+//   inlineImageLimit: 16384,
+//   webpack(config, options) {
+//     return config
+//   }
+// })
